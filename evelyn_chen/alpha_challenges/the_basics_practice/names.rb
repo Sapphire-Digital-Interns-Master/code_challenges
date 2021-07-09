8b412a8e5291e9c13fc55dbf280b813181a64cb2
Evelyn Chen <echen@ECHEN-MAC.local>
2021-07-09 10:03:46 -0400

    I completed the_basics.md

---
+++ evelyn_chen/alpha_challenges/the_basics_practice/names.rb
@@ -0,0 +1,9 @@
+def names
+  'Jackie', 'Hami'
+end
+return names
+
+def caps(hello)
+  hello.upcase
+end
+caps 'hello'
---
+++ evelyn_chen/alpha_challenges/the_basics_practice/turtles.rb
@@ -0,0 +1,8 @@
+x = 0
+
+while x < 4
+  turtles = ["Leonardo", "Raphael", "Michelangelo", "Donatello"]
+  puts turtles[x]
+
+  x = x + 1
+end
