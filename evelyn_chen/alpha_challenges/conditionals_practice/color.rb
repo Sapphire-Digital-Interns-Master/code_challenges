7ccb65ceb500e2cc8f0fd57a5c8ddd0456e77fb3
Evelyn Chen <echen@ECHEN-MAC.local>
2021-07-13 12:19:22 -0400

    I switched from evelyn_chen branch to evelyn_chen_conditionals branch so that only the changes for conditionals are included in the PR.

---
+++ evelyn_chen/alpha_challenges/conditionals_practice/color.rb
@@ -0,0 +1,11 @@
+color = 'Magenta'
+
+if color == 'Magenta'
+  'Magenta is my second favorite color!'
+elsif color == 'Chartreuse'
+  'Chartreuse is my favorite color!'
+elsif color == 'Periwinkle'
+  'Periwinkle os my third favorite color!'
+elsif
+  'I hate that color!'
+end
